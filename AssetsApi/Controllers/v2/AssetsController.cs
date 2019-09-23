afd0ebdf656738976133cb52b8e68db8b6387f35
Emanuel Ceballos <emanuel.ceballos@youbim.com>
2019-09-23 00:53:42 -0300

    Add GatewayApi and AssetsApi projects

---
+++ AssetsApi/Controllers/v2/AssetsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetsApi.Controllers.v2
+{
+    [Route("api/v2/[controller]")]
+    [ApiController]
+    public class AssetsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Version 2", "Asset 1", "Asset 2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Version 2! - Asset {id}!";
+        }
+
+        [HttpGet("{id}/attributes")]
+        public ActionResult<IEnumerable<string>> GetAttributes(int id)
+        {
+            return new string[] { "Version 2", $"Asset {id}!", "Attribute 1", "Attribute 2" };
+        }
+    }
+}
---
+++ AssetsApi/Controllers/v3/AssetsController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetsApi.Controllers.v3
+{
+    [Route("api/v3/[controller]")]
+    [ApiController]
+    public class AssetsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Version 3", "Asset 1", "Asset 2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Version 3! - Asset {id}!";
+        }
+
+        [HttpGet("{id}/attributes")]
+        public ActionResult<IEnumerable<string>> GetAttributes(int id)
+        {
+            return new string[] { "Version 3", $"Asset {id}!", "Attribute 1", "Attribute 2" };
+        }
+    }
+}
